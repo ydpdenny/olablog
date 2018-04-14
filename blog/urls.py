dd3b6cb39f8eb24291a79b56517f02ff1c0c4db7
ydpdenny <dennykim512@gmail.com>
2018-04-14 17:41:32 +0900

    tracking

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^globalbuster/$', views.globalbuster, name='globalbuster'),
+    url(r'^postlist$', views.post_list, name='post_list'),
+    url(r'^$', views.globalbuster, name='globalbuster'),
     url(r'^globalbusteractive/$', views.globalbusteractive, name='globalbusteractive'),
     url(r'^login/$', views.login, name='login'),
 ]
